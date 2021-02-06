b1e1f1b147d95860d298dca107b0b9430304363e
mkanim1 <70426519+mkanim1@users.noreply.github.com>
2021-02-06 14:11:29 -0500

    wargame

--- programs/wargame.py
+++ programs/wargame.py
@@ -1,1 +1,21 @@
-print ("test")
+suits = ["Hearts", "Spades", "Clubs", "Diamonds" ]
+deck = []
+i=1
+for i in range(1,14):
+    if i == 1:
+        value = "A"
+    elif i == 11:
+        value = "J"
+    elif i == 12:
+        value = "Q"
+    elif i == 13:
+        value = "K"
+    else:
+        value = str(i)
+    j = 0
+    for j in range(len(suits)):
+        card = value + " " + suits[j]
+        deck.append(card)
+
+print(deck)
+print("Length of deck", len(deck))
